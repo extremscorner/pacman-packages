1b50cc0b7a342cee7d1d5c1cb57b1f21417d9c13
Extrems <metaradil@gmail.com>
2024-04-09 16:34:45 -0400

    Add libogc2-cmake package

---
+++ libogc2-cmake/libogc.cmake
@@ -0,0 +1,35 @@
+# -----------------------------------------------------------------------------
+# Platform configuration
+
+cmake_minimum_required(VERSION 3.13)
+include_guard(GLOBAL)
+
+# Inherit default devkitPro platform configuration
+include(Platform/Generic-dkP)
+
+# Platform settings
+set(OGC_ARCH_SETTINGS "-m${OGC_MACHINE} -DGEKKO -mcpu=750 -meabi -mhard-float")
+set(OGC_COMMON_FLAGS  "-ffunction-sections")
+set(OGC_LINKER_FLAGS  "-L${OGC_ROOT}/lib/${OGC_SUBDIR} -L${DEVKITPRO}/portlibs/${OGC_CONSOLE}/lib -L${DEVKITPRO}/portlibs/ppc/lib")
+set(OGC_STANDARD_LIBRARIES "${OGC_EXTRA_LIBS} -logc -lm")
+set(OGC_STANDARD_INCLUDE_DIRECTORIES "${OGC_ROOT}/include")
+
+__dkp_init_platform_settings(OGC)
+
+# -----------------------------------------------------------------------------
+# Platform-specific helper utilities
+
+function(ogc_create_dol target)
+    if (NOT ELF2DOL_EXE)
+        message(FATAL_ERROR "Could not find elf2dol: try installing gamecube-tools")
+    endif()
+
+    __dkp_target_derive_name(DOL_OUTPUT ${target} ".dol")
+    add_custom_command(TARGET ${target} POST_BUILD
+        COMMAND "${ELF2DOL_EXE}" "$<TARGET_FILE:${target}>" "${DOL_OUTPUT}"
+        BYPRODUCTS "${DOL_OUTPUT}"
+        COMMENT "Converting ${target} to .dol format"
+        VERBATIM
+    )
+    dkp_set_target_file(${target} "${DOL_OUTPUT}")
+endfunction()
---
+++ libogc2-cmake/ogc-common.cmake
@@ -0,0 +1,47 @@
+cmake_minimum_required(VERSION 3.13)
+
+if("${CMAKE_SYSTEM_NAME}" STREQUAL "NintendoWii")
+    set(OGC_CONSOLE wii)
+    set(OGC_SUBDIR wii)
+    set(OGC_MACHINE rvl)
+elseif("${CMAKE_SYSTEM_NAME}" STREQUAL "NintendoGameCube")
+    set(OGC_CONSOLE gamecube)
+    set(OGC_SUBDIR cube)
+    set(OGC_MACHINE ogc)
+else()
+    message(FATAL_ERROR "Unsupported OGC platform")
+endif()
+
+if(NOT DKP_OGC_PLATFORM_LIBRARY)
+    set(DKP_OGC_PLATFORM_LIBRARY libogc2)
+endif()
+
+# Import devkitPPC toolchain
+include(${CMAKE_CURRENT_LIST_DIR}/devkitPPC.cmake)
+
+list(APPEND CMAKE_TRY_COMPILE_PLATFORM_VARIABLES DKP_OGC_PLATFORM_LIBRARY)
+
+if("${DKP_OGC_PLATFORM_LIBRARY}" STREQUAL "libogc")
+    set(OGC_ROOT "${DEVKITPRO}/libogc")
+elseif("${DKP_OGC_PLATFORM_LIBRARY}" STREQUAL "libogc2")
+    set(OGC_ROOT "${DEVKITPRO}/libogc2")
+else()
+    message(FATAL_ERROR "Unsupported OGC platform library: '${DKP_OGC_PLATFORM_LIBRARY}'")
+endif()
+
+set(DKP_INSTALL_PREFIX_INIT ${DEVKITPRO}/portlibs/${OGC_CONSOLE})
+
+__dkp_platform_prefix(
+    ${DEVKITPRO}/portlibs/${OGC_CONSOLE}
+    ${DEVKITPRO}/portlibs/ppc
+    ${OGC_ROOT}
+)
+
+find_program(PKG_CONFIG_EXECUTABLE NAMES powerpc-eabi-pkg-config HINTS "${DEVKITPRO}/portlibs/${OGC_CONSOLE}/bin")
+if (NOT PKG_CONFIG_EXECUTABLE)
+    message(FATAL_ERROR "Could not find powerpc-eabi-pkg-config: try installing ${OGC_CONSOLE}-pkg-config")
+endif()
+
+find_program(ELF2DOL_EXE NAMES elf2dol HINTS "${DEVKITPRO}/tools/bin")
+find_program(GCDSPTOOL_EXE NAMES gcdsptool HINTS "${DEVKITPRO}/tools/bin")
+find_program(GXTEXCONV_EXE NAMES gxtexconv HINTS "${DEVKITPRO}/tools/bin")
