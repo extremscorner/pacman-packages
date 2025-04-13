c248d5e973c130b1acff63400ccd815aba76b683
Extrems <metaradil@gmail.com>
2025-04-13 17:20:31 -0400

    Add libogc2-dkp-toolchain-vars package

---
+++ libogc2-dkp-toolchain-vars/gamecubevars.sh
@@ -0,0 +1,13 @@
+. ${DEVKITPRO}/devkitppc.sh
+
+export PORTLIBS_PREFIX=${PORTLIBS_ROOT}/gamecube
+export PORTLIBS_PPC=${PORTLIBS_ROOT}/ppc
+export PORTLIBS_CUBE=${PORTLIBS_PREFIX}
+
+export CFLAGS="-O2 -mogc -mcpu=750 -meabi -mhard-float -ffunction-sections -fdata-sections"
+export CXXFLAGS="${CFLAGS}"
+export CPPFLAGS="-D__GAMECUBE__ -I${PORTLIBS_CUBE}/include -I${PORTLIBS_PPC}/include -I${DEVKITPRO}/libogc2/include"
+export LDFLAGS="-L${PORTLIBS_CUBE}/lib -L${PORTLIBS_PPC}/lib -L${DEVKITPRO}/libogc2/lib/cube"
+export LIBS="-logc -lm"
+
+export PATH=${PORTLIBS_CUBE}/bin:${PORTLIBS_PPC}/bin:$PATH
---
+++ libogc2-dkp-toolchain-vars/wiivars.sh
@@ -0,0 +1,13 @@
+. ${DEVKITPRO}/devkitppc.sh
+
+export PORTLIBS_PREFIX=${PORTLIBS_ROOT}/wii
+export PORTLIBS_PPC=${PORTLIBS_ROOT}/ppc
+export PORTLIBS_WII=${PORTLIBS_PREFIX}
+
+export CFLAGS="-O2 -mrvl -mcpu=750 -meabi -mhard-float -ffunction-sections -fdata-sections"
+export CXXFLAGS="${CFLAGS}"
+export CPPFLAGS="-D__WII__ -I${PORTLIBS_WII}/include -I${PORTLIBS_PPC}/include -I${DEVKITPRO}/libogc2/include"
+export LDFLAGS="-L${PORTLIBS_WII}/lib -L${PORTLIBS_PPC}/lib -L${DEVKITPRO}/libogc2/lib/wii"
+export LIBS="-logc -lm"
+
+export PATH=${PORTLIBS_WII}/bin:${PORTLIBS_PPC}/bin:$PATH
